d54cec98f9c7bc395e296680f1f2dd8b69ef085a
TooniOlaniyan <106449840+TooniOlaniyan@users.noreply.github.com>
2022-06-03 13:28:39 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+ const toggler = document.querySelector('.toggle-button');
+ const menu = document.querySelector('.nav-items');
+ toggler.addEventListener('click',()=>{
+     menu.classList.toggle('change');
+
+ })
